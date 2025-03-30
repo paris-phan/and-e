d696a586f3fbbcf2a83c913ef13d0d639ea36f66
Paris Phan <37759794+paris-phan@users.noreply.github.com>
2025-03-30 11:38:20 -0400

    added hand tracking environment

---
+++ hand_tracking/hand_tracking/Assets/DisplayWebcam.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class DisplayWebcam : MonoBehaviour
+{
+
+    public RenderTexture renderTexture;
+    public Material paddingMaterial;
+    private WebCamTexture tex;
+    
+    private Renderer rend;
+    
+    void Start ()
+    {
+
+        Renderer rend = this.GetComponentInChildren<Renderer>();
+        if (rend != null)
+        {
+            tex = new WebCamTexture(3840,1080,60);
+            rend.material.mainTexture = tex;
+            
+            tex.Play();
+        }
+        else
+        {
+            Debug.LogError("RawImage not set");
+        }
+    }
+    
+    void Update ()
+    {
+        // when the webcam feed is updated, update the render texture
+        if (tex && tex.didUpdateThisFrame)
+        { 
+            Graphics.Blit(tex, renderTexture);
+        }
+    }
+}
